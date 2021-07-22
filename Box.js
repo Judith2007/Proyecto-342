f64296b0c85fb9188fd7fb39d876cfbee2ae11cb
Judith2007 <84209053+Judith2007@users.noreply.github.com>
2021-07-22 11:47:05 -0500

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box extends BaseClass{
   constructor(x, y) {
      super(x,y,50,50);
-     this.image= loadImage("sprites/wood1.png")
+     this.image= loadImage("wood1.png")
   }
   display(){
      
